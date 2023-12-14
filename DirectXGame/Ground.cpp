eb52139289aed9f9544a58b627222b7e40b48172
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2023-12-14 18:34:14 +0900

    Ground

---
+++ DirectXGame/Ground.cpp
@@ -0,0 +1,22 @@
+﻿#include "Ground.h"
+#include <ImGuiManager.h>
+void Ground::Initialize(Model* model) {
+
+	assert(model);
+
+	model_ = model;
+
+	worldtransform_.Initialize();
+
+	worldtransform_.scale_ = {
+
+	    0.5f,
+	    0.5f,
+	    0.5f,
+
+	};
+}
+
+void Ground::Update() { worldtransform_.UpdateMatrix(); }
+
+void Ground::Draw(ViewProjection& viewProjection) { model_->Draw(worldtransform_, viewProjection); }
